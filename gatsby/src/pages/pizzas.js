de76708a3fb7df37e053677d9eb6f8dfc63df57c
fbenites <fbenites@hartinc.com>
2021-07-19 08:23:14 -0400

    Configuring GraphQL

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const PizzasPage = () => (
-  <>
-    <p>Hey, I'm the Pizzas page!</p>
-  </>
-);
+import PizzaList from '../components/PizzaList';
+
+const PizzasPage = ({ data }) => {
+  const pizzas = data.pizzas.nodes;
+  return (
+    <>
+      <p>Hey! There are {pizzas.length} pizzas!!</p>
+      <PizzaList pizzas={pizzas} />
+    </>
+  );
+};
 export default PizzasPage;
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
